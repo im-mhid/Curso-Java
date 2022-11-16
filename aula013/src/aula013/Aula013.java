3f64ee4c11ead9ed5c28e8f92f967ff717670f96
Matheus Dias <hmatheus031@gmail.com>
2022-11-15 21:21:27 -0300

    Aula 13 completa
    Polimorfismo de sobrecarga

---
+++ aula013/src/aula013/Aula013.java
@@ -0,0 +1,18 @@
+package aula013;
+
+public class Aula013 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cachorro c = new Cachorro();
+		c.reagir("Olá");
+		c.reagir("Vai apanhar");
+		c.reagir(11, 45);
+		c.reagir(21, 00);
+		c.reagir(true);
+		c.reagir(false);
+		c.reagir(2, 12.5f);
+		c.reagir(17, 4.5f);
+	}
+
+}
---
+++ aula013/src/aula013/Lobo.java
@@ -0,0 +1,11 @@
+package aula013;
+
+public class Lobo extends Mamifero {
+
+	@Override
+	public void emitirSom() {
+		// TODO Auto-generated method stub
+		System.out.println("Auuuuuuuuuuuu!");
+	}
+	
+}
